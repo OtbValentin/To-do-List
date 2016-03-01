fa34d45ff1cbed5bfa88e860d213776d7c62f952
polycirrus <polycirrus@gmail.com>
2016-03-01 17:57:01 +0300

    Add mock BL services

---
+++ Sources/Epam.TodoManager.BusinessLogic.Mock/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.TodoManager.BusinessLogic.Mock.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
---
+++ Sources/Epam.TodoManager.BusinessLogic.Mock/MockContext.cs
@@ -0,0 +1,20 @@
+﻿using Epam.TodoManager.BusinessLogic.Mock.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.TodoManager.BusinessLogic.Mock
+{
+    class MockContext : DbContext
+    {
+        public MockContext() : base("MockDB")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
