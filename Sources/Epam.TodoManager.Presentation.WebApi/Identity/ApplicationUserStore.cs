a08faf61ae0771a3d8426357ce0088b839ff96d8
polycirrus <polycirrus@gmail.com>
2016-03-05 15:57:46 +0300

    Add demo actions, tweak user store

--- Sources/Epam.TodoManager.Presentation.WebApi/Identity/ApplicationUserStore.cs
+++ Sources/Epam.TodoManager.Presentation.WebApi/Identity/ApplicationUserStore.cs
@@ -4,56 +4,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Threading.Tasks;
+using Epam.TodoManager.BusinessLogic.UserService;
+using Epam.TodoManager.BusinessLogic.RoleService;
+using Mock = Epam.TodoManager.BusinessLogic.Mock;
+using Epam.TodoManager.DomainModel.Entities;
 
 namespace Epam.TodoManager.Presentation.WebApi.Identity
 {
     //TODO: implement additional interfaces (IXxxStore) if necessary
+    //      reimplement mapping
     public class ApplicationUserStore : IUserStore<ApplicationUser, int>, IUserPasswordStore<ApplicationUser, int>
     {
+        private IUserService userService;
+        private IRoleService roleService;
+
+        public ApplicationUserStore()
+        {
+            userService = new Mock.UserService();
+            roleService = new Mock.RoleService();
+        }
+
         public Task CreateAsync(ApplicationUser user)
         {
-            throw new NotImplementedException();
+            userService.Create(user.UserName, user.PasswordHash);
+            return Task.CompletedTask;
         }
 
         public Task DeleteAsync(ApplicationUser user)
         {
-            throw new NotImplementedException();
+            userService.Delete(user.Id);
+            return Task.CompletedTask;
         }
 
         public void Dispose()
         {
             return;
-            //throw new NotImplementedException();
         }
 
         public Task<ApplicationUser> FindByIdAsync(int userId)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(ToApplicationUser(userService.Find(userId)));
         }
 
         public Task<ApplicationUser> FindByNameAsync(string userName)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(ToApplicationUser(userService.Find(userName)));
         }
 
         public Task<string> GetPasswordHashAsync(ApplicationUser user)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrWhiteSpace(user.PasswordHash))
+                return Task.FromResult(user.PasswordHash);
+
+            var domainUser = userService.Find(user.Id);
+            return Task.FromResult(domainUser?.PasswordHash);
         }
 
         public Task<bool> HasPasswordAsync(ApplicationUser user)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public Task SetPasswordHashAsync(ApplicationUser user, string passwordHash)
         {
-            throw new NotImplementedException();
+            user.PasswordHash = passwordHash;
+            return Task.CompletedTask;
         }
 
         public Task UpdateAsync(ApplicationUser user)
         {
             throw new NotImplementedException();
+        }
+
+        private static User ToDomainUser(ApplicationUser appUser)
+        {
+            if (appUser == null)
+                return null;
+
+            return new User(appUser.Id, appUser.UserName, appUser.PasswordHash, null, null);
+        }
+
+        private static ApplicationUser ToApplicationUser(User domainUser)
+        {
+            if (domainUser == null)
+                return null;
+
+            return new ApplicationUser()
+            {
+                Id = domainUser.Id,
+                UserName = domainUser.Email,
+                PasswordHash = domainUser.PasswordHash
+            };
         }
     }
 }
---
+++ Sources/Epam.TodoManager.Presentation.WebApi/Models/DemoModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epam.TodoManager.Presentation.WebApi.Models
+{
+    public class DemoModel
+    {
+        public string StringProp { get; set; }
+        public int IntProp { get; set; }
+    }
+
+    public class RegistrationData
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
