9238d9ba12bfd4c20758f0a1d6a58170ab5b7b13
polycirrus <polycirrus@gmail.com>
2016-03-05 15:57:46 +0300

    Add demo actions, tweak user store

--- Sources/Epam.TodoManager.Presentation.WebApi/Controllers/DemoController.cs
+++ Sources/Epam.TodoManager.Presentation.WebApi/Controllers/DemoController.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Epam.TodoManager.Presentation.WebApi.Models;
+using Epam.TodoManager.Presentation.WebApi.Identity;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
 
 namespace Epam.TodoManager.Presentation.WebApi.Controllers
 {
     public class DemoController : ApiController
     {
+        private ApplicationUserManager manager;
+
+        public DemoController()
+        {
+            manager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+        }
+
         [Route("api/Demo/1")]
         [HttpGet]
         public string NoAuth()
@@ -16,10 +28,40 @@ namespace Epam.TodoManager.Presentation.WebApi.Controllers
             return "no secret";
         }
 
-        [Route("api/Demo/2")]
+        [Route("api/Demo/2"), HttpGet, Authorize]
         public string Auth()
         {
             return "secret";
+        }
+
+        [Route("api/Demo/3")]
+        [HttpGet]
+        public DemoModel Demo3()
+        {
+            return new DemoModel()
+            {
+                StringProp = "string1",
+                IntProp = 115
+            };
+        }
+        
+        [Route("api/Demo/4")]
+        [HttpPost]
+        public void Demo4([FromBody] DemoModel model)
+        {
+            return;
+        }
+
+        [Route("api/Demo/Register")]
+        [HttpPost]
+        public async Task<IHttpActionResult> Register([FromBody] RegistrationData data)
+        {
+            var result = await manager.CreateAsync(new ApplicationUser() { UserName = data.Email }, data.Password);
+
+            if (result.Succeeded)
+                return Ok();
+            else
+                return BadRequest(result.Errors.Aggregate((errors, error) => $"{error}, \n {error}"));
         }
     }
 }
