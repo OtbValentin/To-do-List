7679a1f86ea17c03dfc15ed1f507fd4b481fd9d5
OtbValentin <valentin.pilipchuk.24@gmail.com>
2016-03-15 13:37:45 +0300

    Reimplement user repository

--- Sources/Epam.TodoManager.DataAccess.EF/Repositories/EFUserRepository.cs
+++ Sources/Epam.TodoManager.DataAccess.EF/Repositories/EFUserRepository.cs
@@ -30,12 +30,41 @@ namespace Epam.TodoManager.DataAccess.EF.Repositories
             return mapper.Map<User>(user);
         }
 
+        public override User Find(int key)
+        {
+            DB.User user = context.Set<DB.User>().Include(u => u.Profile).FirstOrDefault(u => u.Id == key);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return mapper.Map<User>(user);
+        }
+
+        public override void Create(User entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            DB.User efEntity = mapper.Map<DB.User>(entity);
+            efEntity.ListCollection = new DB.TodoListCollection();
+            context.Set<DB.User>().Add(efEntity);
+        }
+
         public override void Update(User entity)
         {
             DB.User user = mapper.Map<DB.User>(entity);
 
-            context.Entry(user).State = EntityState.Modified;
-            context.Entry(user.Profile).State = EntityState.Modified;
+            DB.User dbUser = context.Set<DB.User>().Include(u => u.Profile).FirstOrDefault(u => u.Id == entity.Id);
+            context.Entry(dbUser).State = EntityState.Modified;
+            context.Entry(dbUser.Profile).State = EntityState.Modified;
+
+            dbUser.Email = user.Email;
+            dbUser.PasswordHash = user.PasswordHash;
+            dbUser.Profile.Name = entity.Profile.Name;
         }
     }
 }
